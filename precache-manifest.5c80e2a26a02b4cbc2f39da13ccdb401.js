71c7a2603d70796aab85dd9a0e9436129a1844fd
misterx <216417+misterx@users.noreply.github.com>
2020-07-26 18:15:41 +0300

    initial

---
+++ precache-manifest.5c80e2a26a02b4cbc2f39da13ccdb401.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc09850540f9589dd31bc3c5385e5c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5722217923d54b8cd8ab",
+    "url": "./static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "6c6d09f28651974b9766",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5722217923d54b8cd8ab",
+    "url": "./static/js/2.88d5fb8c.chunk.js"
+  },
+  {
+    "revision": "e719d5d20086ca53686c880329eb2f8f",
+    "url": "./static/js/2.88d5fb8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6d09f28651974b9766",
+    "url": "./static/js/main.2d839f2e.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
